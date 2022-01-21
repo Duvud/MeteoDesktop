f393765696bb606f56f957a77e427ea0f1a0cad2
Duvud <davidbrunalvarez@gmail.com>
2022-01-22 00:12:37 +0100

    Lista de balizas conseguida, conseguir datos de balizas sin parsear conseguido

--- MeteoDesktopSolution/Data/DataParser.cs
+++ MeteoDesktopSolution/Data/DataParser.cs
@@ -23,5 +23,27 @@ namespace MeteoDesktopSolution.Data
             return personList.ToArray();
         }
 
+        public static async void getStationData(String stationId) {
+            
+            DateTime localDate = DateTime.Now;
+            String month = localDate.Month.ToString();
+            if (month.Length == 1) {
+                month = "0" + month;
+            }
+            String day = localDate.Day.ToString();
+            String year = localDate.Year.ToString();
+            String requestUrl = $"https://www.euskalmet.euskadi.eus/vamet/stations/readings/{stationId}/{year}/{month}/{day}/readingsData.json";
+            Debug.WriteLine(requestUrl);
+            var client = new HttpClient { BaseAddress = new Uri(requestUrl) };
+            var responseMessage = await client.GetAsync("", HttpCompletionOption.ResponseContentRead);
+            var resultData = await responseMessage.Content.ReadAsStringAsync();
+            Debug.WriteLine(resultData);
+            dynamic stationReadingsJson = JsonConvert.DeserializeObject(resultData);
+            foreach (var obj in stationReadingsJson)
+            {
+                Debug.WriteLine(obj);
+            }
+        }
+
     }
 }
--- MeteoDesktopSolution/Form1.cs
+++ MeteoDesktopSolution/Form1.cs
@@ -10,40 +10,31 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        Control.CheckForIllegalCrossThreadCalls = false;
+        LoadComboBox();
     }
 
-    private void button1_Click(object sender, EventArgs e)
-    {
-        
-    }
-
-    private async void button2_Click(object sender, EventArgs e)
-    {
+    private async void LoadComboBox() {
         Station[] stationList = (await DataParser.getStations().ConfigureAwait(false));
         DataTable stationDt = new DataTable("StationList");
         stationDt.Columns.Add("id");
         stationDt.Columns.Add("nombre");
-        for (int i = 0; i < stationList.Length; i++) {
+        for (int i = 0; i < stationList.Length; i++)
+        {
             stationDt.Rows.Add(stationList[i].id, stationList[i].name);
         }
-        for (int j = 0; j < stationDt.Rows.Count; j++) {
-            this.comboBox1.Items.Add(stationDt.Rows[j]["id"].ToString() + " " + stationDt.Rows[j]["nombre"].ToString());
+        for (int j = 0; j < stationDt.Rows.Count; j++)
+        {
+            comboBox1.Items.Add(stationDt.Rows[j]["id"].ToString() + " " + stationDt.Rows[j]["nombre"].ToString());
         }
-        /*
-         *DataTable dt = new DataTable("Terrorists");
-            dt.Columns.Add("FirstName");
-            dt.Columns.Add("LastName");
-            
-            dt.Rows.Add("Osama", "Bin Laden");
-            dt.Rows.Add("Saddam", "Hoessein");
-            dt.Rows.Add("George", "Bush");
 
-            for (int j = 0; j < dt.Rows.Count; j++)
+    }
 
-            {
-                string text = dt.Rows[j]["FirstName"].ToString() + " " + dt.Rows[j]["LastName"].ToString();
-                this.comboBox1.Items.Add(text);
-            }
-         */
+    private void button1_Click(object sender, EventArgs e)
+    {
+        if (comboBox1.SelectedItem != null) {
+            String stationId = this.comboBox1.SelectedItem.ToString().Split(" ")[0];
+            DataParser.getStationData(stationId);
+        }
     }
 }
